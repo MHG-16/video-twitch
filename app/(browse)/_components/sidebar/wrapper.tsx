43116db29ce33c26bb50527fa8c3bea61a410dae
MHG1816 <hammaguibeneess@gmail.com>
2024-01-01 11:54:35 +0100

    Hydration Error

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -2,6 +2,10 @@
 
 import { cn } from "@/lib/utils"
 import { useSidebar } from "@/store/use-sidebar"
+import { useEffect, useState } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
+import { useIsClient } from "usehooks-ts";
 
 interface WrapperProps {
     children: React.ReactNode
@@ -9,9 +13,17 @@ interface WrapperProps {
 
 export const Wrapper = ({children} : WrapperProps) => {
     const { collapsed } = useSidebar((state) => state)
+    const isClient = useIsClient();
+
+    if (!isClient) return (
+        <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+            <ToggleSkeleton />
+            <RecommendedSkeleton />
+        </aside>
+    );
     return(
         <aside className={cn(
-            "fixed left-0 flex flex-col w-60 h-full", 
+            "fixed left-0 flex flex-col w-[70px] lg:w-60 h-full", 
             "bg-background border-r border-[#2D2E35] z-50",
             collapsed && "w-[70px]"
             )}>
